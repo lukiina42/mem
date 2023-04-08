425945b250841cc65f50b3ba708d3996308bab9a
lukiina42 <lukas.vala25@gmail.com>
2023-04-08 16:24:05 +0200

    Display date of creation / last update on mem

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,8 +1,9 @@
 import HomeContentWrapper from "@/components/home/HomeContentWrapper";
 import { cookies } from "next/headers";
 import { decode } from "next-auth/jwt";
+import { Mem, MemBE } from "@/types";
 
-const getMems = async () => {
+const getMems = async (): Promise<Mem[]> => {
   const jwtObject = await decode({
     token: cookies().get("next-auth.session-token")?.value,
     secret: process.env.NEXTAUTH_SECRET!,
@@ -18,7 +19,31 @@ const getMems = async () => {
     console.log(response);
     throw new Error(`The fetch wasn't successful ${response.status}`);
   }
-  return await response.json();
+  let memsBE: MemBE[] = await response.json();
+  let mems: Mem[] = memsBE.map((mem) => {
+    const createdTime = new Date(mem.createdDate);
+    const updatedTime = new Date(mem.updatedDate);
+
+    const time =
+      createdTime.getTime() == updatedTime.getTime()
+        ? createdTime
+        : updatedTime;
+
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    //todo use timeZone as a locale somehow
+    const formatedTime = new Intl.DateTimeFormat("cs-CZ", {
+      day: "numeric",
+      month: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    }).format(time);
+    return {
+      ...mem,
+      lastUpdated: formatedTime,
+    };
+  });
+  return mems;
 };
 
 export default async function page() {
