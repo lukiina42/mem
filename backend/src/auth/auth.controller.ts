4a9fb2112bd51a4c77e583fcf689ed2115a28783
lukiina42 <lukas.vala25@gmail.com>
2023-04-24 22:37:20 +0200

    Middleware check JWT token, create auth controller

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
+
+@Controller('/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async login(@Request() req) {
+    const token = await this.authService.login(req.user);
+    return {
+      token: token.access_token,
+      user: req.user,
+    };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/profile')
+  getProfile() {
+    return true;
+  }
+}
