bb300951e0fa6b1afe650b0169136e6d9fe9c782
Lukas Vala <lukas.vala25@gmail.com>
2023-11-26 00:16:35 +0100

    Update BE secret key

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -27,7 +27,7 @@ export class AuthService {
     const payload = { email: user.email, sub: user.id, roles: user.roles };
     return {
       access_token: this.jwtService.sign(payload, {
-        secret: this.configService.get('JWT_SECRET'),
+        secret: 'secretKey',
       }),
     };
   }
