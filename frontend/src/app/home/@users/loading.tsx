06f95c179b7a8db3c1846071c5f8a1507be7a987
Lukas Vala <lukas.vala25@gmail.com>
2023-11-25 22:29:10 +0100

    Parallel routes loading states, upgrade to next 14

--- frontend/src/app/home/@users/loading.tsx
+++ frontend/src/app/home/@users/loading.tsx
@@ -2,7 +2,7 @@ import LoadingSpinner from '@/utilComponents/Loading';
 
 export default function loading() {
   return (
-    <div className="w-full h-full flex justify-center mt-4">
+    <div className="w-full h-full flex justify-center mt-16">
       <LoadingSpinner />
     </div>
   );
